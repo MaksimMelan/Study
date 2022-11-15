f5946a73b94fcbbf3d6d8f658b0d91fa5804dede
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2022-11-15 20:03:17 +0500

    independen decision

---
+++ Цикл while/Цикл while/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+// пишем программу покторая поздравит нас столько раз сколько нам лет
+int age; // My it's old.
+age = Convert.ToInt32(Console.ReadLine());
+while(age > 0)
+{
+    Console.WriteLine("Привет");
+    age--; 
+}
+Console.ReadKey();
