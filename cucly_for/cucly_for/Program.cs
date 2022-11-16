98d3b9c0d2cc323c1d3e6976f5b69c6c95264469
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2022-11-16 17:53:31 +0500

    cucly for  -  from 0 to 9

---
+++ cucly_for/cucly_for/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+for(int i = 0; i < 10;i++) // for-  в () находится три блока 1-переменная, 2-условие, 3-правило изменениия этой перерменной.
+{
+    Console.WriteLine(i);
+}
+Console.ReadKey();
