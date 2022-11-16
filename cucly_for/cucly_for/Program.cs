0074947c96c4f0c7f0950f1f94c63656bfa79d9f
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2022-11-16 17:58:34 +0500

    + with step 2

--- cucly_for/cucly_for/Program.cs
+++ cucly_for/cucly_for/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-for(int i = 10; i > 0;i--) // for-  в () находится три блока 1-переменная, 2-условие, 3-правило изменениия этой перерменной.
+for(int i = 0; i < 10;i+= 2) // for-  в () находится три блока 1-переменная, 2-условие, 3-правило изменениия этой перерменной.
+//  + with step 2
 {
     Console.WriteLine(i);
 }
