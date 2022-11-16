a3af1ea16bbd9b0cb8e513bdb6b25209a1906387
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2022-11-16 18:40:40 +0500

    a program that wishes you a happy birthday

--- cucly_for/cucly_for/Program.cs
+++ cucly_for/cucly_for/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-for(int i = 0; i < 10;i+= 2) // for-  в () находится три блока 1-переменная, 2-условие, 3-правило изменениия этой перерменной.
-//  + with step 2
+int age = 10; // a program that wishes you a happy birthday
+
+
+for (int i = 0; i < age; i++) // for-  в () находится три блока 1-переменная, 2-условие, 3-правило изменениия этой перерменной.
 {
-    Console.WriteLine(i);
+    Console.WriteLine("Happy birthday " + i);
 }
 Console.ReadKey();
