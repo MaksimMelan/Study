a5b0811d2bc8ad3dd368d5dfcd3a105b765f986d
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2022-11-16 20:58:25 +0500

    from 0 to 100 , it's even numbers

--- cucly_for/cucly_for/Program.cs
+++ cucly_for/cucly_for/Program.cs
@@ -2,7 +2,7 @@
 int age = 10; // a program that wishes you a happy birthday
 
 
-for (int i = 0; i < age; i++) // for-  в () находится три блока 1-переменная, 2-условие, 3-правило изменениия этой перерменной.
+for (int i = 0; i <= 100; i+=2) // for-  в () находится три блока 1-переменная, 2-условие, 3-правило изменениия этой перерменной.
 {
     Console.WriteLine("Happy birthday " + i);
 }
