01ed22e48bdcba8b64c5f62ee71e1789dd582f48
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2022-12-06 16:08:09 +0500

    Число равно 5, либо равно 10

---
+++ if3/if3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace if3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Напишите программу, в которой  пользователь  вводит с клавиатуры чилсо. 
+             * Если числов либо равно 5 либо равно 10, то программа выводит "Число равно 5, либо равно 10". 
+             * Иначе программа выводит сообщение "Неизвестное число"
+             */
+
+            int num;
+
+            Console.WriteLine("Введите число: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num == 5 || num == 10)
+            {
+                Console.WriteLine("Число равно 5, либо равно 10.");
+            }
+            else
+            {
+                Console.WriteLine("Неизвестное число");
+            }
+            Console.ReadKey();
+        }
+    }
+}
