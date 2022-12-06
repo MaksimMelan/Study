75cc29f1900e2f6ff1649d9b8b7b1335b5d346f4
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2022-12-06 15:45:49 +0500

    Если число одновременно больше 5 и меньше 10, то программа выводит "Число больше 5 и меньше 10"

---
+++ if/if/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace practice_with_cycles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Напишите консольную программу, в которую пользователь вводит с клавиатуры два числа.
+            * А программа сарвнивает два введённых числа и выводит на консоль результат сравнения
+            * (два числа равны, первое число больше второго или первое число меньше второго).
+            */
+
+            int num1;
+            int num2;
+
+            Console.WriteLine("Ведите первое число: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ведите второе число");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine($"Первое число {num1} больше второго {num2}. ");
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine($"Первое число {num1} меньше второго {num2}. ");
+            }
+            else
+            {
+                Console.WriteLine($"Чилсо {num1} и {num2} равны. ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ if/if2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace if2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Напишите программу, в которую пользователь вводить с клавиатуры число. 
+             * Если число одновременно больше 5 и меньше 10, то программа выводит "Число больше 5 и меньше 10".
+             * Иначе программа выводит сообщение "Неизвестное число".
+             */
+
+            int num;
+
+            Console.WriteLine("Ведите числo: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if(num > 5 && num < 10)
+            {
+                Console.WriteLine($"Число больше {num} больше 5 и меньше 10. ");
+            }
+            else
+            {
+                Console.WriteLine("Неизвестное число! ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
